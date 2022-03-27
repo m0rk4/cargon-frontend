fac7bf0fcf401e9332b14cc062ed73c046e4e783
Mark Putyato <65854558+m0rk4@users.noreply.github.com>
2022-03-27 21:14:34 +0300

    Sync tables with backend, refactoring  (#6)
    * Refactored transport application logic * added fetching hook * sync with back * Added orders and users management logic

--- src/pages/orders/OrdersPage.tsx
+++ src/pages/orders/OrdersPage.tsx
@@ -1,76 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { MainLayout } from '../../layouts/MainLayout';
-import { notification, Table, TableColumnsType } from 'antd';
-import { UserItem } from '../users/UsersPage';
+import { notification, Table } from 'antd';
 import { CheckCircleOutlined } from '@ant-design/icons';
 import { Order } from './models/order.interface';
 import { GeoLocation } from './models/location.interface';
-
-export interface OrderItem extends Order {
-  key: string;
-}
-
-const sampleOrders: Order[] = [
-  {
-    id: 1,
-    createdAt: new Date().toDateString(),
-    updatedAt: new Date().toDateString(),
-    owner: {
-      id: 3,
-      firstName: 'Vlad',
-      lastName: 'Brown',
-      email: 'test3@mail.com',
-      isRegistrationApproved: true,
-      isActive: true,
-    },
-    fromLocation: {
-      city: { name: 'Moscow' },
-      street: { name: 'Independence' },
-      home: 123,
-    },
-    toLocation: {
-      city: { name: 'Moscow' },
-      street: { name: 'Eastern' },
-      home: 321,
-    },
-    status: {
-      statusName: 'Awaiting Approval',
-    },
-  },
-];
+import useFetching from '../../hooks/useFetch';
+import { User } from '../users/models/user.interface';
 
 const OrdersPage: React.FC = () => {
-  const [loading, setLoading] = useState(true);
-  const [orders, setOrders] = useState<OrderItem[]>([]);
+  const { data, isLoading, hasError, setData, setLoading } = useFetching<
+    Order[]
+  >('order/not-approved', []);
 
-  useEffect(() => {
-    async function fetchUnapprovedOrders() {
-      /**
-       * TODO: replace with real getUnapprovedOrders call.
-       */
-      const orders: Order[] = await new Promise((resolve) => {
-        setTimeout(() => {
-          resolve(sampleOrders);
-        }, 1000);
-      });
-
-      setOrders(
-        orders.map(
-          (order): OrderItem => ({ ...order, key: order.id.toString() }),
-        ),
-      );
-      setLoading(false);
-    }
-
-    fetchUnapprovedOrders();
-  }, []);
-
-  const renderUser = (user: UserItem) => (
-    <a>{`${user.firstName} ${user.lastName}`}</a>
+  const renderUser = ({ firstName, lastName }: User) => (
+    <a>{`${firstName} ${lastName}`}</a>
   );
-  const renderLocation = (location: GeoLocation) => (
-    <div>{`${location.city.name}, ${location.street.name}, ${location.home}`}</div>
+
+  const renderLocation = ({ city, street, home }: GeoLocation) => (
+    <div>{`${city.name}, ${street.name}, ${home}`}</div>
   );
+
+  const renderAction = (id: number) => (
+    <a onClick={() => onApprove(id)}>Approve</a>
+  );
+
+  const onApprove = async (id: number) => {
+    const response = await approveOrder(id);
+    if (!response.ok) return;
+
+    removeOrder(id);
+    openNotification();
+  };
+
+  const approveOrder = async (id: number) => {
+    setLoading(true);
+    const response = await fetch(`order/${id}/approve`, {
+      method: 'PUT',
+    });
+    setLoading(false);
+    return response;
+  };
+
+  const removeOrder = (id: number) => {
+    const index = data.findIndex((order) => order.id === id);
+    if (index === -1) return;
+
+    setData([...data.slice(0, index), ...data.slice(index + 1)]);
+  };
 
   const openNotification = () => {
     notification.info({
@@ -81,11 +57,11 @@ const OrdersPage: React.FC = () => {
     });
   };
 
-  const columns: TableColumnsType<any> | undefined = [
+  const columns = [
     {
       title: 'Owner',
       key: 'owner',
-      dataIndex: 'owner',
+      dataIndex: 'user',
       render: renderUser,
     },
     {
@@ -113,37 +89,26 @@ const OrdersPage: React.FC = () => {
     {
       title: 'Actions',
       key: 'actions',
-      render: (record: OrderItem) => {
-        const onClick = async () => {
-          setLoading(true);
-          /**
-           * TODO: replace with real API call.
-           */
-          await fetch('https://jsonplaceholder.typicode.com/todos/1');
-
-          setLoading(false);
-
-          const index = orders.findIndex((order) => order.id === record.id);
-          if (index === -1) return;
-
-          setOrders([...orders.slice(0, index), ...orders.slice(index + 1)]);
-          openNotification();
-        };
-
-        return <a onClick={onClick}>Approve</a>;
-      },
+      dataIndex: 'id',
+      render: renderAction,
     },
   ];
 
   return (
     <MainLayout>
-      <Table
-        title={() => 'Orders'}
-        bordered
-        loading={loading}
-        columns={columns}
-        dataSource={orders}
-      />
+      {data && (
+        <Table
+          title={() => 'Orders'}
+          bordered
+          loading={isLoading}
+          columns={columns}
+          dataSource={data.map((order) => ({
+            ...order,
+            key: order.id.toString(),
+          }))}
+        />
+      )}
+      {hasError && <h1>Something went wrong...</h1>}
     </MainLayout>
   );
 };
--- src/pages/transport-applications/models/transport-application.interface.ts
+++ src/pages/transport-applications/models/transport-application.interface.ts
@@ -3,5 +3,5 @@ import { User } from '../../users/models/user.interface';
 export interface TransportApplication {
   id: number;
   driver: User;
-  documentUuid: string;
+  documentUid: string;
 }
